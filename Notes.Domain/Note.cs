efc054b1d354ff1109660cbb4f6d3fb3e45d5896
Dima0410 <dvbotalov@gmail.com>
2021-11-17 19:01:47 +0500

    Добавьте файлы проекта.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Domain
+{
+    // Здесь будет содержаться описание сущностей заметок, бизнес-логика относящаяся к сущности заметок
+    // (Уровень Core => Domain - Interprices logic, относится к общему направлению деятельности компании, очень обобщенная логика)
+
+    class Note
+    {
+        public Guid UserID { get; set; }
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+    // Notes.Applications: Здесь будет вся логика запросов получения, создания, редактирования заметок и валидации запросов
+    // Notes.Persistence: Здесь будет описание взаимодействия с БД
+}
